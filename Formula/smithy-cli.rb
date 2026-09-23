341ef3a226106ceb80fc499e0af5ad59c31f589e
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2026-09-23 12:04:10 -0700

    Autocorrect Ruby formatting
    `brew style` reported 106 offenses across the tap, which drowned out
    real findings. Apply the autocorrectable ones so later commits carry a
    readable diff.
    
    Mechanical only, via `brew style --fix`:
    
    - 2-space indentation in both files
    - double-quoted strings
    - drop the redundant `# -*- coding: utf-8 -*-` magic comment
    - add `frozen_string_literal` / `typed` sigils and `.freeze` on CONFIG_DIR
    - collapse `unless/else` into an `if/else` assignment
    - `assert_predicate ..., :exist?` -> `assert_path_exists`
    
    The one hand edit is moving the ConfigProvider class comment above the
    class, which `Style/Documentation` wants and cannot autocorrect.
    
    No behavior change.

--- Formula/smithy-cli.rb
+++ Formula/smithy-cli.rb
@@ -1,58 +1,57 @@
-# -*- coding: utf-8 -*-
-require_relative '../ConfigProvider/config_provider'
+require_relative "../ConfigProvider/config_provider"
 
 class SmithyCli < Formula
-    $config_provider = ConfigProvider.new('smithy-cli')
-    desc "Smithy CLI - A CLI for building, validating, querying, and iterating on Smithy models"
-    homepage "https://smithy.io"
-    version $config_provider.version
+  $config_provider = ConfigProvider.new("smithy-cli")
+  desc "Smithy CLI - A CLI for building, validating, querying, and iterating on Smithy models"
+  homepage "https://smithy.io"
+  version $config_provider.version
 
+  if OS.mac?
+    if Hardware::CPU.intel?
+      url "#{$config_provider.root_url}-darwin-x86_64.zip"
+      sha256 $config_provider.sierra_hash
+    elsif Hardware::CPU.arm?
+      url "#{$config_provider.root_url}-darwin-aarch64.zip"
+      sha256 $config_provider.arm64_big_sur_hash
+    end
+  elsif OS.linux?
+    if Hardware::CPU.intel?
+      url "#{$config_provider.root_url}-linux-x86_64.zip"
+      sha256 $config_provider.linux_hash
+    elsif Hardware::CPU.arm?
+      url "#{$config_provider.root_url}-linux-aarch64.zip"
+      sha256 $config_provider.linux_arm_hash
+    end
+  end
+
+  def install
+    # install everything in archive into libexec, so that
+    # the contents are private to homebrew, which means it won't try
+    # to symlink anything in this directory automatically
+    libexec.install Dir["*"]
+    # create a symlink to the private executable
+    bin.install_symlink "#{libexec}/bin/smithy" => "smithy"
+  end
+
+  def post_install
+    # brew relocates dylibs and assigns different ids, which is problematic since
+    # we package a runtime image ourselves
     if OS.mac?
-      if Hardware::CPU.intel?
-        url "#{$config_provider.root_url}-darwin-x86_64.zip"
-        sha256 $config_provider.sierra_hash
-      elsif Hardware::CPU.arm?
-        url "#{$config_provider.root_url}-darwin-aarch64.zip"
-        sha256 $config_provider.arm64_big_sur_hash
-      end
-    elsif OS.linux?
-      if Hardware::CPU.intel?
-        url "#{$config_provider.root_url}-linux-x86_64.zip"
-        sha256 $config_provider.linux_hash
-      elsif Hardware::CPU.arm?
-        url "#{$config_provider.root_url}-linux-aarch64.zip"
-        sha256 $config_provider.linux_arm_hash
+      Dir["#{libexec}/lib/**/*.dylib"].each do |dylib|
+        chmod 0664, dylib
+        MachO::Tools.change_dylib_id(dylib, "@rpath/#{File.basename(dylib)}")
+        # we also need to resign the dylibs, so that their ad-hoc signatures are not invalid
+        MachO.codesign!(dylib)
+        chmod 0444, dylib
       end
     end
+    # call warmup command to generate the jsa
+    system "#{bin}/#{$config_provider.bin}" + " warmup"
+  end
 
-    def install
-        # install everything in archive into libexec, so that
-        # the contents are private to homebrew, which means it won't try
-        # to symlink anything in this directory automatically
-        libexec.install Dir["*"]
-        # create a symlink to the private executable
-        bin.install_symlink "#{libexec}/bin/smithy" => "smithy"
-    end
-
-    def post_install
-        # brew relocates dylibs and assigns different ids, which is problematic since
-        # we package a runtime image ourselves
-        if OS.mac?
-            Dir["#{libexec}/lib/**/*.dylib"].each do |dylib|
-                chmod 0664, dylib
-                MachO::Tools.change_dylib_id(dylib, "@rpath/#{File.basename(dylib)}")
-                # we also need to resign the dylibs, so that their ad-hoc signatures are not invalid
-                MachO.codesign!(dylib)
-                chmod 0444, dylib
-            end
-        end
-        # call warmup command to generate the jsa 
-        system "#{bin}/#{$config_provider.bin}" " warmup"
-    end
-
-    test do
-        assert_predicate lib/"#{$config_provider.bin}.jsa", :exist?
-        assert_match $config_provider.version, shell_output("#{bin}/#{$config_provider.bin} --version")
-        assert_match "Usage: #{$config_provider.bin}", shell_output("#{bin}/#{$config_provider.bin} --help")
-    end
+  test do
+    assert_path_exists lib/"#{$config_provider.bin}.jsa"
+    assert_match $config_provider.version, shell_output("#{bin}/#{$config_provider.bin} --version")
+    assert_match "Usage: #{$config_provider.bin}", shell_output("#{bin}/#{$config_provider.bin} --help")
+  end
 end
